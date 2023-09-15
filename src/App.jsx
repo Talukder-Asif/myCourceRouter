c8406f40a2677ab7b86628bcda133a55769b0c91
Talukder-Asif <asif.talukder.aj.10@gmail.com>
2023-09-15 13:15:12 +0600

    After cart body done

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,13 @@ function App() {
     .then((ref) => ref.json())
     .then((data) => setAllCource(data));
   },[]);
+
+  let courceCradit = 20;
+
   return (
     <>
     <h1 className='text-[#1C1B1B] text-3xl font-bold py-7 text-center'>Course Registration</h1>
-    <main className='md:flex'>
+    <main className='md:flex md:gap-3'>
       <div className='w-3/4 grid gap-3 md:grid-cols-2 lg:grid-cols-3 CourceConteiner'>
 
           {AllCource.map((Indivisualcource)=>(
@@ -28,13 +31,20 @@ function App() {
               <p className='flex text-[#737272] font-normal text-sm items-center gap-1'><TbBrandCashapp/>  Price : {Indivisualcource.price} </p>
               <p className='flex text-[#737272] font-normal text-sm items-center gap-1'><BsBook/>  Credit : {Indivisualcource.credit_hour}hr </p>
             </div>
-            <button className='bg-[#2f80ed] rounded-md py-1 text-white w-full'>Select</button>
+            <button className='bg-[#2f80ed]  rounded-md py-1 text-white w-full'>Select</button>
           </div>
           ))}
 
       </div>
-      <div className='Cart w-1/4'>
-          hi
+      <div className='Cart bg-white h-max rounded-2xl w-1/4 p-5'>
+          <h3 className='text-[#2f80ed] my-3 text-lg font-bold'>Credit Hour Remaining : {courceCradit} hr</h3>
+          <hr></hr>
+          <h3 className='text-[#1C1B1B] text-xl font-bold my-3'>Course Name</h3>
+
+          <hr></hr>
+          <p className='text-[#737272] my-2 text-base font-medium'>Total Credit Hour : </p>
+          <hr></hr>
+          <p className='text-[#737272] my-2 text-base font-semibold'>Total Price : USD</p>
       </div>
     </main>
     </>
